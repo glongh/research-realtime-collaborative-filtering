5fe263fc4b7801bd2817c791032056785ce07f29
glongh <glongh@gmail.com>
2019-03-02 16:05:08 -0500

    mapping the data to the url string

--- src/indexer.py
+++ src/indexer.py
@@ -1,7 +1,15 @@
 import csv
 import socket
+HOST = 'localhost'
+PORT = 3000 
 
-with open('data/ml-100k.csv') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    for row in csv_reader:
-        print(f'\t{row[0]} - {row[1]} - {row[2]}')
+with open('data/ml-100k.csv', newline='') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        if(row['rating'] > 3):
+            payload = bytearray("HTTP GET /item/{1}/user/{2}".format(row['movieId'], row['userId']),'UTF-8') 
+            # Simulate multiples connections 
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((HOST, PORT))
+            s.sendall(payload) 
+            s.close()
